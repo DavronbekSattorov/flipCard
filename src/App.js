212277f42926c63945fe21fe298fdc29641f7d11
Davronbek <davsattor@gmail.com>
2021-12-15 06:34:24 +0100

    Card

--- src/App.js
+++ src/App.js
@@ -10,43 +10,54 @@ const App = () => {
         const arrayOfWord = [
           {
             name : 'book',
-            id : 1
+            id : 1,
+            className: 'word'
             },
             {
               name : 'computer',
-              id : 2
+              id : 2,
+              className: 'word'
             },
             {
               name : 'phone',
-              id : 3
+              id : 3,
+              className: 'word'
             },
             {
               name : 'pen',
-              id : 4
+              id : 4,
+              className: 'word'
             },
             {
               name : 'cup',
-              id : 5
+              id : 5,
+              className: 'word'
             },
             {
               name : 'paper',
-              id : 6
+              id : 6,
+              className: 'word'
             },
             {
               name : 'car',
-              id : 7
+              id : 7,
+              className: 'word'
             },
             {
               name : 'licence',
-              id : 8
+              id : 8,
+              className: 'word'
             },
             {
               name : 'tablet',
-              id : 9
+              id : 9,
+              className: 'word'
             },
             {
               name : 'phone',
-              id : 10
+              id : 10,
+              className: 'word'
+
             }
         ]
 
@@ -69,14 +80,38 @@ const App = () => {
   
         
   // console.log(arrayWords)
-  const [storeId, setStoreId] = useState();
+  const [storeId, setStoreId] = useState([]);
   //const [compareArray]
-  
-  const getId = (id) => {
-    setStoreId(id);
+
+useEffect(()=> {
+  if(storeId.length === 2) {
+    //let filtered = [];
+    let filteredArray = data.filter(el => el.customId === storeId[0]);
+    let filteredArray1 = data.filter(el => el.customId === storeId[1]);
+    let filtered = [filteredArray,filteredArray1];
+
+    if(filteredArray[0].id === filteredArray1[0].id) {
+      console.log('yes they are same')
+      let array = data.map(el => el.id === filteredArray1[0].id ? {...el, className:'word2'}: el)
+      setData(array)
+    } else if (filteredArray[0].id !== filteredArray1[0].id) {
+      let array = data.map(el => el.id === filteredArray1[0].id ? {...el, className:'word'}: el)
+      setData(array)
+    }
+    
+    setStoreId([]);
+    
+  }
+}, [storeId])
+
+  const getId = (customId) => {
+    
+    setStoreId([...storeId, customId]);
+    let array = data.map(el => el.customId === customId ? {...el, className:'word2'}: el)
+      setData(array)
   }
 
-
+//console.log(storeId)
   console.log(storeId,'storeId');
   console.log(data)
   return (
@@ -86,7 +121,7 @@ const App = () => {
           data.length && data.map(word => {
             return(
               <div className='box' onClick={() => getId(word.customId)}>
-                <p className={storeId === word.customId ? 'word2' : 'word'}>{word.name}</p>
+                <p className={word.className}>{word.name}</p>
               </div>
             )
           })
